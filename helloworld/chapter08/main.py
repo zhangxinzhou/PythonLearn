1b4a6ae146f61033a96c68c10602f065a55545cc
aabbcc5050 <aabbcc5050@qq.com>
2018-09-02 21:05:15 +0800

    learning

---
+++ helloworld/chapter08/main.py
@@ -0,0 +1,7 @@
+import chapter08.bmi
+import chapter08.bmi as bb
+from chapter08.bmi import fun_bmi
+
+chapter08.bmi.fun_bmi('赵云', 1.74, 73.8)
+bb.fun_bmi('赵云', 1.74, 73.8)
+fun_bmi('赵云', 1.74, 73.8)
