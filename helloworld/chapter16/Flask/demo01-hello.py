fbe8ffd3455b530728b4a4cd5d131b2db3030229
aabbcc5050@qq.com <aabbcc5050@qq.com>
2018-11-25 22:16:28 +0800

    001

---
+++ helloworld/chapter16/Flask/demo01-hello.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello world!'
+
+
+@app.route('/test')
+def nihao():
+    return 'aaa'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
